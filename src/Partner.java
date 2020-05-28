73e9a284f37428b9eccb4eb137bfac07166b627f
EUFHMarcel2708 <66075408+EUFHMarcel2708@users.noreply.github.com>
2020-05-28 16:30:54 +0200

    First Commit

---
+++ src/Partner.java
@@ -0,0 +1,27 @@
+public class Partner {
+	
+	private String name = "";
+	private boolean kooperation = false;
+	
+	public Partner(String aName) {
+		name = aName;
+		kooperation = true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public boolean isKooperation() {
+		return kooperation;
+	}
+
+	public void setKooperation(boolean kooperation) {
+		this.kooperation = kooperation;
+	}
+
+}
